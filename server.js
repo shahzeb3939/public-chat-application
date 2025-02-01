c0a8814b0d56de5c544417871a06e14777d94135
Shahzeb Khan <shahzeb.khan@emumba.com>
2025-02-02 01:00:16 +0500

    Add db connection string to .env

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const cors = require('cors');
 const db = require('./db');
 
 const app = express();
-const PORT = process.env.PORT || 4000;
+const PORT = process.env.PORT || 3000;
 
 app.use(cors());
 app.use(bodyParser.json());
